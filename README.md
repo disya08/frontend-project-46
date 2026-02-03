59b649cf60e259dac80e91937caf25d2174ad50d
Denis Ivanov <disatrophic@gmail.com>
2026-02-03 22:06:58 +0700

    step 4

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/disya08/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/disya08/frontend-project-46/actions)
+## Example
+
+```bash
+$ gendiff file1.json file2.json
+{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
 import parseFile from './parsers.js';
+import _ from 'lodash';
+
+const buildDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = _.sortBy(_.union(keys1, keys2));
+  
+  const diff = allKeys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    
+    if (!_.has(data1, key)) {
+      return { key, type: 'added', value: value2 };
+    }
+    if (!_.has(data2, key)) {
+      return { key, type: 'removed', value: value1 };
+    }
+    if (value1 === value2) {
+      return { key, type: 'unchanged', value: value1 };
+    }
+    return { key, type: 'changed', value: value1, value2 };
+  });
+  
+  return diff;
+};
+
+const formatDiff = (diff) => {
+  const lines = diff.map((item) => {
+    switch (item.type) {
+      case 'added':
+        return `  + ${item.key}: ${item.value}`;
+      case 'removed':
+        return `  - ${item.key}: ${item.value}`;
+      case 'unchanged':
+        return `    ${item.key}: ${item.value}`;
+      case 'changed':
+        return `  - ${item.key}: ${item.value}\n  + ${item.key}: ${item.value2}`;
+      default:
+        return '';
+    }
+  });
+  
+  return `{\n${lines.join('\n')}\n}`;
+};
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
-  
-  console.log('File 1:', data1);
-  console.log('File 2:', data2);
-  
-  return 'Comparison result will be here';
+  const diff = buildDiff(data1, data2);
+  return formatDiff(diff);
 };
 
 export default genDiff;
