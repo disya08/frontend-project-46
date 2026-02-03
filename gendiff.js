c25c4fcea046d401b4f759fb016c5abdbaeb2696
Denis Ivanov <disatrophic@gmail.com>
2026-02-03 21:34:24 +0700

    step 2

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,9 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('1.0.0', '-V, --version')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
+  
